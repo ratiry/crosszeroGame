bc7acae13bd9ab97d01eaa39e2029c12308704dd
ratiry <ratiry@yandex.ru>
2023-01-20 15:49:28 +0300

    Headers and footers are temprorary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.module.scss';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import classes from './App.module.scss';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <div className={classes.App}>
+          <Header/>
+          <div></div>
+          <Footer/>
+        </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import classes from './Header.module.scss';
+import AppClasses from './../../App.module.scss';
+import logo from './../../images/Tic-Tac-toe.png';
+let Header=(props)=>{
+  return(
+    <header className={classes.header}>
+    <div className={AppClasses.container}>
+      <div className={classes.logo_container}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>
+        <span className={classes.name}>Crosszero Game</span>
+      </div>
+    </div>
+  </header>
+  )
+}
+export default Header;
