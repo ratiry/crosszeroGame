ddd56d41fb3e2900d04673da61c7ed1cc0348448
ratiry <ratiry@yandex.ru>
2023-01-20 19:05:55 +0300

    made temprorary welcome page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.module.scss';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import classes from './App.module.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Welcome from './components/Content/Welcome/Welcome';
 function App() {
+  
   return (
     <BrowserRouter>
         <div className={classes.App}>
           <Header/>
-          <div></div>
+          <div className={classes.Content}>
+            <Routes>
+              <Route path={'/'} element={<Welcome/>} />
+              
+            </Routes>
+          </div>
           <Footer/>
         </div>
     </BrowserRouter>
---
+++ src/components/Content/Welcome/Welcome.jsx
@@ -0,0 +1,25 @@
+import classes from './Welcome.module.scss';
+import Appclasses from './../../../App.module.scss';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+let Welcome=()=>{
+  let [redirect,Setredirect] = useState(false);
+  if(!redirect){
+    return(
+      <div className={classes.Welcome + ' ' + Appclasses.container }>
+        <p>
+          Hello, BlaBlaBla
+        </p>
+        <p>
+          Do you dare to challenge the dumbest computer player of tic tac toe?
+        </p>
+        <button className={classes.Start_button} onClick={()=> Setredirect(true)}><span>Just Do it</span></button>
+      </div>
+    )
+  }else{
+    return(
+      <Navigate to={'/Game'}/>
+    )
+  }
+}
+export default Welcome;
