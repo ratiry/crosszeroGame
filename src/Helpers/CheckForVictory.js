b40979327a1bfbc9119f5ef7ecdfbab9ce7958f6
ratiry <ratiry@yandex.ru>
2023-01-24 15:50:42 +0300

    finished detection of victory

--- src/Helpers/CheckForVictory.js
+++ src/Helpers/CheckForVictory.js
@@ -1,20 +1,44 @@
 
+
 let conditipnsFunction=(index,cells,occuppied_Cells,player,rows)=>{
   let result = null;
+  let definedHorizontalCondition=cells[occuppied_Cells[index].id +2] !==undefined & cells[occuppied_Cells[index].id +1] !==undefined;;
+  let definedVerticalCondition=cells[occuppied_Cells[index].id +rows] !==undefined & cells[occuppied_Cells[index].id +rows+rows] !==undefined;;
+  let definedLeftDiagonalCondition=cells[occuppied_Cells[index].id +rows+1] !== undefined & cells[occuppied_Cells[index].id +2*(rows+1)]!==undefined;
+  let definedRightDiagonal=cells[occuppied_Cells[index].id +rows-1] !== undefined & cells[occuppied_Cells[index].id +2*(rows-1)]!==undefined;
+
+
+
   
-  let definedCondition=cells[occuppied_Cells[index].id +2] !==undefined & cells[occuppied_Cells[index].id +1] !==undefined;
-  if(definedCondition & cells[occuppied_Cells[index].id].whose===player ){
-    let onOneRowHorizontalCondition=Math.floor(cells[occuppied_Cells[index].id].id/rows) ===Math.floor(cells[occuppied_Cells[index].id+1].id/rows) & Math.floor(cells[occuppied_Cells[index].id].id/rows ===Math.floor(cells[occuppied_Cells[index].id+2].id/rows));
-    let horizontalCpndition=cells[occuppied_Cells[index].id +1].whose==player & cells[occuppied_Cells[index].id +2].whose==player;
-    if(horizontalCpndition){
-      debugger;
-      if(onOneRowHorizontalCondition){
-        debugger;
+  if(cells[occuppied_Cells[index].id].whose===player){
+    if(definedHorizontalCondition){
+
+      let onOneRowHorizontalCondition=Math.floor(cells[occuppied_Cells[index].id].id/rows) ===Math.floor(cells[occuppied_Cells[index].id+1].id/rows) & Math.floor(cells[occuppied_Cells[index].id].id/rows ===Math.floor(cells[occuppied_Cells[index].id+2].id/rows));
+      let horizontalCpndition=cells[occuppied_Cells[index].id +1].whose==player & cells[occuppied_Cells[index].id +2].whose==player;    
+      if(horizontalCpndition & onOneRowHorizontalCondition){
         result=player;
-        return result;
-      } 
+      }
+    }
+    if(definedVerticalCondition){
+      let verticalCondition=cells[occuppied_Cells[index].id +rows].whose==player & cells[occuppied_Cells[index].id +rows+rows].whose==player;
+      if(verticalCondition){
+        result=player;
+      }
+    }
+    if(definedLeftDiagonalCondition){
+      let LeftDiagonalCondition = cells[occuppied_Cells[index].id +rows+1].whose==player & cells[occuppied_Cells[index].id +2*(rows+1)].whose==player;
+      if(LeftDiagonalCondition){
+        result=player;
+      }
+    }
+    if(definedRightDiagonal){
+      let RightDiagonalCondition = cells[occuppied_Cells[index].id +rows-1].whose==player & cells[occuppied_Cells[index].id +2*(rows-1)].whose==player;
+      if(RightDiagonalCondition){
+        result=player;
+      }
     }
   }
+  return result;
 }
  let CheckForVictory=(cells,occuppied_Cells,rows)=>{
   let result = null;
