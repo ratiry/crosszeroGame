9840a33d47d46fcf4e1141af315e23c18fa8d8ed
ratiry <ratiry@yandex.ru>
2023-01-24 21:49:46 +0300

    fixed bug with RightDiagonal in defining victory

--- src/Helpers/CheckForVictory.js
+++ src/Helpers/CheckForVictory.js
@@ -16,24 +16,46 @@ let conditipnsFunction=(index,cells,occuppied_Cells,player,rows)=>{
       let onOneRowHorizontalCondition=Math.floor(cells[occuppied_Cells[index].id].id/rows) ===Math.floor(cells[occuppied_Cells[index].id+1].id/rows) & Math.floor(cells[occuppied_Cells[index].id].id/rows ===Math.floor(cells[occuppied_Cells[index].id+2].id/rows));
       let horizontalCpndition=cells[occuppied_Cells[index].id +1].whose==player & cells[occuppied_Cells[index].id +2].whose==player;    
       if(horizontalCpndition & onOneRowHorizontalCondition){
+        debugger;
         result=player;
       }
     }
     if(definedVerticalCondition){
       let verticalCondition=cells[occuppied_Cells[index].id +rows].whose==player & cells[occuppied_Cells[index].id +rows+rows].whose==player;
       if(verticalCondition){
+        debugger;
         result=player;
       }
     }
     if(definedLeftDiagonalCondition){
       let LeftDiagonalCondition = cells[occuppied_Cells[index].id +rows+1].whose==player & cells[occuppied_Cells[index].id +2*(rows+1)].whose==player;
       if(LeftDiagonalCondition){
+        debugger;
         result=player;
       }
     }
     if(definedRightDiagonal){
       let RightDiagonalCondition = cells[occuppied_Cells[index].id +rows-1].whose==player & cells[occuppied_Cells[index].id +2*(rows-1)].whose==player;
-      if(RightDiagonalCondition){
+      let rowsArray=[cells[occuppied_Cells[index].id].id/rows,cells[occuppied_Cells[index].id+rows-1].id/rows,cells[occuppied_Cells[index].id+2*(rows-1)].id/rows];
+      let NotOnOneRow=true;
+      for(let i=0;i<rowsArray.length;i++){
+        if((rowsArray[i]+1)%rows==0){
+          rowsArray[i]=rowsArray[i]+1;
+          break;
+        }
+      }
+      for(let i=0;i<rowsArray.length;i++){
+        rowsArray[i] = Math.ceil(rowsArray[i]);
+      }
+      for(let i=0;i<rowsArray.length;i++){
+        for(let ii=0;ii<rowsArray.length;ii++){
+          if(rowsArray[i] == rowsArray[ii] & i!==ii){
+            NotOnOneRow=false;
+          }
+        }
+      }
+      if(RightDiagonalCondition & NotOnOneRow){
+        debugger;
         result=player;
       }
     }
